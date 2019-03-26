4f29fdcda308e4852ff6daded9469f3c1e2a6d92
haimyshajahan <haimy.shajahan1@gmail.com>
2019-03-26 20:14:43 +0530

    average prgm

---
+++ avge.sh
@@ -0,0 +1,10 @@
+echo "Enter first number: "
+read a
+echo "Enter second number: "
+read b
+echo "Enter third number: "
+read c
+sum=`expr $a + $b + $c`
+avg=`expr $sum / 3`
+echo "Sum = $sum"
+echo "Average = $avg"
